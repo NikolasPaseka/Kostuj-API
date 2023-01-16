9fe9153e99ca8000a64c8856f5d1f524663b54ef
Nikolas Paseka <nikolas.paseka7@gmail.com>
2023-01-16 11:11:16 +0100

    db connection to atlas

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,17 +1,23 @@
 import mongoose, * as Mongoose from "mongoose";
-//require('dotenv').config();
+import { strict as assert } from 'assert';
+import { load } from 'ts-dotenv';
+
+const env = load({
+    ATLAS_DB_URL: String,
+});
 
 let connection: Mongoose.Connection;
 
 export const connect = () => {
     //const url = process.env.MONGO_CONNECTION_STRING || "mongodb://localhost:27017/kostuj";
-    const url = "mongodb://localhost:27017/kostuj";
+    //const url = "mongodb://localhost:27017/kostuj";
+    const url = env.ATLAS_DB_URL;
 
     if (connection) {
         return;
     }
 
-    Mongoose.connect('mongodb://localhost:27017/kostuj')
+    Mongoose.connect(url)
     .then(() => {
         console.log('mongo connection open');
         transformSchemasToClient();
