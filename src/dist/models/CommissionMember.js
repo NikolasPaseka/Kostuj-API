c0c01e16c7af1a443c97e48e676bd5c1b3bf0d0d
Nikolas Paseka <89791795+NikolasPaseka@users.noreply.github.com>
2023-12-01 15:26:03 +0100

    Delete src/dist directory

--- src/dist/models/CommissionMember.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CommissionMember = void 0;
-const mongoose_1 = require("mongoose");
-const commissionMemberSchema = new mongoose_1.Schema({
-    catalogueId: { type: mongoose_1.Schema.Types.ObjectId, required: true, ref: "Catalogue" },
-    userId: { type: mongoose_1.Schema.Types.ObjectId, required: true, ref: "User" }
-});
-exports.CommissionMember = (0, mongoose_1.model)("CommissionMember", commissionMemberSchema);
